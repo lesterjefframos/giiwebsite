bcf8d183c02731aff46e86b3593c87606928e93d
lesterjefframos <35057921+lesterjefframos@users.noreply.github.com>
2022-08-09 15:34:34 +0800

    Update smtp.js

--- js/smtp.js
+++ js/smtp.js
@@ -1,6 +1,6 @@
 function sendEmail (){
     Email.send({
-        Host : "smtp25.elasticemail.com",
+        Host : "smtp.elasticemail.com",
         Username : "relay@giiph.com",
         Password : "4ADDDBE63A276C4057879AA47FA9063A6669",
         To : 'giitestmail2022@gmail.com',
@@ -14,8 +14,3 @@ function sendEmail (){
   message => alert(message)
     );
 }
-
-//Body : "Name: " + document.getElementById("name").value
-//+ "<br> Email: " + document.getElementById("email").value
-//+ "<br> Contact No.: " + document.getElementById("contact").value
-//+ "<br> Message: " + document.getElementById("message").value
